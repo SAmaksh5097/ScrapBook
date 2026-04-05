4048f4cfdaacf5d7431f745ed029d588b5f58946
SAmaksh5097 <samaksharzare.cs2004@gmail.com>
2026-04-05 20:22:56 +0530

    added routing

--- frontend/src/components/Herosection.jsx
+++ frontend/src/components/Herosection.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-
+import { Link } from 'react-router-dom'
 const Herosection = () => {
   return (
     <div className='relative flex min-h-[calc(100vh-72px)] items-center bg-black px-4 py-16 text-white md:px-10'>
@@ -9,7 +9,9 @@ const Herosection = () => {
           <div className='mt-8 w-full max-w-2xl border-l-2 border-white/70 pl-4 md:mt-10 md:pl-6'>
             <p className='text-lg leading-relaxed md:text-2xl'>Easy to build, effortless to share, and made to relive every special moment with your loved ones.</p>
             <div>
+              <Link to={'/dashboard'}>
               <button className='mt-6 rounded-full bg-white px-6 py-2 text-sm font-semibold text-black transition hover:bg-black hover:text-white hover:shadow-[0_0_0_1px_white_inset] md:px-8 md:py-3 md:text-base cursor-pointer'>Get Started</button>
+              </Link>
             </div>
           </div>
         </div>

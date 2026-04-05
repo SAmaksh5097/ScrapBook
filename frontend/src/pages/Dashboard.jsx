d26455179b355ebb6941002c4c00c41e32d967be
SAmaksh5097 <samaksharzare.cs2004@gmail.com>
2026-04-05 20:32:21 +0530

    added + button

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,5 +1,5 @@
 import DashboardCard from '../components/DashboardCard'
-
+import { Plus } from 'lucide-react'
 const Dashboard = () => {
   const trips = [
     { id: 1, title: 'Summer 2024 Trip', date: 'Date' },
@@ -13,9 +13,11 @@ const Dashboard = () => {
   return (
     <div className='min-h-screen bg-black text-white py-10 px-4 md:px-10'>
       <div className='mx-auto w-full max-w-7xl'>
-        <div className='mb-8 flex flex-col gap-2'>
+        <div className='mb-8 flex  items-center justify-between'>
           <h1 className='text-2xl font-semibold tracking-wide md:text-4xl'>My Scrapbook Memories</h1>
-          
+          <button className='flex gap-2 border p-2 rounded bg-white text-black cursor-pointer hover:bg-gray-300 items-center transition '>
+            <Plus/>
+            New memory</button>
         </div>
 
         <div className='grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4'>

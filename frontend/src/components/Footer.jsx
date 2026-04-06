8b8a950739e49f03dd937351af46c6e2dfbd0445
SAmaksh5097 <samaksharzare.cs2004@gmail.com>
2026-04-07 01:01:01 +0530

    Added link in footer

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -5,7 +5,10 @@ const Footer = () => {
     <div className='absolute bottom-0 w-full'>
         <div className='border-t border-white/35 bg-[#131313] backdrop-blur px-4 text-center text-sm text-white/70 md:px-10 md:py-6'>
           <p>© {new Date().getFullYear()} ScrapBook.</p>
-          <h1>WebApp made with ❤️ by SAmaksh</h1>
+          <h1>WebApp made with ❤️ by <span>
+              <a href="https:samaksh-arzare.vercel.app" target="_blank" rel="noopener noreferrer" className='hover:underline'>SAmaksh</a>
+            </span>
+          </h1>
         </div>
       
     </div>
